aa64bd8140fc6ac7af09d6f9e4d687b216a789d2
sndrpt <103604366+sndrpt@users.noreply.github.com>
2022-12-17 14:46:35 +0100

    Created the structure, installed React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Sandra Pytlik and is
+        <a href="">open-sourced</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
